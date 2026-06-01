65cd4afb5c1a82711cbb869e6ba2e6792095bebd
Antoniii28 <TU_CORREO_DE_GITHUB>
2026-05-31 19:20:58 -0600

    Mejora del modelo y pruebas de clasificación

--- ml/modelo_ml.py
+++ ml/modelo_ml.py
@@ -7,6 +7,8 @@ df = pd.read_csv("data/dataset_prediccion_escolar.csv")
 # MOSTRAR PRIMERAS FILAS
 print(df.head())
 
+print("\nCantidad de estudiantes:", len(df))
+
 # VARIABLES DE ENTRADA
 X = df[[
     "Asistencia (%)",
@@ -16,6 +18,9 @@ X = df[[
     "Parcial 3",
     "Promedio Final"
 ]]
+
+print("\nVariables utilizadas:")
+print(X.columns.tolist())
 
 # VARIABLE OBJETIVO
 y = df["Nivel de Riesgo"]
@@ -28,15 +33,36 @@ modelo.fit(X, y)
 
 # NUEVO ESTUDIANTE
 nuevo_estudiante = pd.DataFrame([{
-    "Asistencia (%)": 85,
-    "Tareas": 80,
+    "Asistencia (%)": 95,
+    "Tareas": 90,
+    "Parcial 1": 92,
+    "Parcial 2": 94,
+    "Parcial 3": 91,
+    "Promedio Final": 92
+},
+{
+    "Asistencia (%)": 80,
+    "Tareas": 75,
     "Parcial 1": 70,
     "Parcial 2": 75,
-    "Parcial 3": 90,
-    "Promedio Final": 78
-}])
+    "Parcial 3": 78,
+    "Promedio Final": 75
+},
+{
+    "Asistencia (%)": 55,
+    "Tareas": 50,
+    "Parcial 1": 45,
+    "Parcial 2": 50,
+    "Parcial 3": 40,
+    "Promedio Final": 48
+}
+
+])
 
 # PREDICCIÓN
-resultado = modelo.predict(nuevo_estudiante)
+resultados = modelo.predict(nuevo_estudiante)
 
-print("Nivel de riesgo predicho:", resultado[0])
+print("\nRESULTADOS:")
+
+for i, riesgo in enumerate(resultados):
+    print(f"Estidiante {i+1}: {riesgo}")

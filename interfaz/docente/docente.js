e76340529bed7afd66b9e037e732ac4830a18b7b
Antoniii28 <TU_CORREO_DE_GITHUB>
2026-06-20 17:48:52 -0600

    Avance

--- interfaz/docente/docente.js
+++ interfaz/docente/docente.js
@@ -30,3 +30,54 @@ document
     alert("Asistencia guardada correctamente");
 
 });
+
+const dashboardBtn = document.getElementById("dashboard-btn");
+const asistenciaBtn = document.getElementById("asistencia-btn");
+const calificacionesBtn = document.getElementById("calificaciones-btn");
+const riesgoBtn = document.getElementById("riesgo-btn");
+
+const dashboardSection = document.getElementById("dashboard-section");
+const asistenciaSection = document.getElementById("asistencia-section");
+const calificacionesSection = document.getElementById("calificaciones-section");
+const riesgoSection = document.getElementById("riesgo-section");
+
+function ocultarTodo() {
+
+    dashboardSection.style.display = "none";
+    asistenciaSection.style.display = "none";
+    calificacionesSection.style.display = "none";
+    riesgoSection.style.display = "none";
+
+}
+
+dashboardBtn.addEventListener("click", () => {
+
+    ocultarTodo();
+
+    dashboardSection.style.display = "block";
+
+});
+
+asistenciaBtn.addEventListener("click", () => {
+
+    ocultarTodo();
+
+    asistenciaSection.style.display = "block";
+
+});
+
+calificacionesBtn.addEventListener("click", () => {
+
+    ocultarTodo();
+
+    calificacionesSection.style.display = "block";
+
+});
+
+riesgoBtn.addEventListener("click", () => {
+
+    ocultarTodo();
+
+    riesgoSection.style.display = "block";
+
+});
